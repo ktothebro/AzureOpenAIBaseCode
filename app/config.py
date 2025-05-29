b44570ca845f6fbc7b4672d5221a3a7602ce4fe5
Kai Brown <kbrown@affirma.com>
2025-05-29 13:57:02 -0700

    clear out

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-API_KEY = os.getenv('AZURE_OPENAI_KEY')
-ENDPOINT = os.getenv('AZURE_OPENAI_ENDPOINT')
--- app/main.py
+++
@@ -1,5 +0,0 @@
-from classifier import classify_ticket
-
-if __name__ == '__main__':
-    ticket = input('Enter IT ticket description: ')
-    print('Category:', classify_ticket(ticket))

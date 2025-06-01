e4f4586a29240fd97381d77cb2310ee5e8540fd0
ktothebro <62850066+ktothebro@users.noreply.github.com>
2025-06-01 13:04:01 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from classifier import classify_ticket
-
-if __name__ == '__main__':
-    ticket = input('Enter IT ticket description: ')
-    print('Category:', classify_ticket(ticket))
+#from classifier import classify_ticket
+from config import API_KEY, ENDPOINT
+print("Key:", API_KEY)
+print("Endpoint:", ENDPOINT)
+#if __name__ == '__main__':
+#    ticket = input('Enter IT ticket description: ')
+#    print('Category:', classify_ticket(ticket))
